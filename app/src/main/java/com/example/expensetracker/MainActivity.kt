bd8034714207d16c9dc4a21202e529fb0c8a56f1
Awais Ashraf <mhawais504@gmail.com>
2024-07-25 19:43:20 +0500

    completed navigation and also home desgin incompleted

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -1,20 +1,76 @@
 package com.example.expensetracker
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.expensetracker.databinding.ActivityMainBinding
+import com.example.expensetracker.databinding.FragmentLoginBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            itemNavigation(item)
+        }
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.signupFragment2, R.id.authFragment, R.id.loginFragment2 -> {
+                    binding.bottomNav.visibility = View.GONE
+                }
+
+                else -> {
+                    binding.bottomNav.visibility = View.VISIBLE
+                }
+            }
+        }
 
 
+    }
+
+
+
+
+    private fun itemNavigation(item: MenuItem): Boolean {
+       return when (item.itemId) {
+            R.id.homeFragment -> {
+                navController.navigate(R.id.homeFragment)
+
+                true
+            }
+
+            R.id.historyFragment -> {
+                navController.navigate(R.id.historyFragment)
+                true
+            }
+
+            R.id.transactionFragment -> {
+                navController.navigate(R.id.transactionFragment)
+                true
+            }
+
+            else -> false
+        }
 
 
     }
 }
+
+
